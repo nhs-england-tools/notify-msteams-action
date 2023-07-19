caab4e552f6b91a41526d1d8ef560e9f0729e500
walteck <walteck@users.noreply.github.com>
2023-07-19 07:42:11 +0100

    initial commit for the action (#1)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    The initial commit - copying the format from
    [actions/typescript-action](https://github.com/actions/typescript-action)
    to implement the framework plus updating main.ts to enable the sending
    of a notification to MS Teams.
    
    ## Context
    
    This is to enable teams to easily send notifications from their build
    pipelines to Microsoft Teams.
    
    ## Type of changes
    
    - [ ] Refactoring (non-breaking change)
    - [X] New feature (non-breaking change which adds functionality)
    - [X] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [X] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [X] I have followed the code style of the project
    - [^] I have added tests to cover my changes
    - [X] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [X] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import * as github from '@actions/github'
+import * as core from '@actions/core'
+import axios from 'axios'
+import {buildMessageCard} from './messagecard'
+import {escapeMarkdown} from './markdownhelper'
+
+async function run(): Promise<void> {
+  try {
+    const githubToken = core.getInput('github-token', {required: true})
+    const teamsWebhookUrl = core.getInput('teams-webhook-url', {required: true})
+    const messageTitle = core.getInput('message-title', {required: true})
+    const messageBody = core.getInput('message-text', {required: true})
+    const messageColour =
+      core.getInput('message-colour', {required: false}) || '00cbff'
+
+    const [owner, repoName] = (process.env.GITHUB_REPOSITORY || '').split('/') // https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+    const sha = process.env.GITHUB_SHA || ''
+    const runNumber = process.env.GITHUB_RUN_NUMBER || ''
+    const runId = process.env.GITHUB_RUN_ID || ''
+    const repoUrl = `https://github.com/${owner}/${repoName}`
+    const repoBranch = process.env.GITHUB_REF_NAME || ''
+
+    const octokit = github.getOctokit(githubToken)
+    const params = {owner, repo: repoName, ref: sha}
+    const commit = await octokit.rest.repos.getCommit(params)
+    const author = commit.data.author
+    let avatar_url = 'https://avatars.githubusercontent.com/u/105098969'
+    let login = 'Not provided'
+    let author_url = 'Not provided'
+    if (author) {
+      if (author.avatar_url) {
+        avatar_url = author.avatar_url
+      }
+      login = author.login
+      author_url = author.html_url
+    }
+
+    const messageCard = buildMessageCard(
+      escapeMarkdown(messageTitle),
+      escapeMarkdown(messageBody),
+      messageColour,
+      runNumber,
+      runId,
+      repoName,
+      repoUrl,
+      repoBranch,
+      avatar_url,
+      login,
+      author_url
+    )
+
+    const response = await axios.post(teamsWebhookUrl, messageCard, {
+      headers: {'Content-Type': 'application/json'}
+    })
+
+    core.debug(`Response: ${JSON.stringify(response.data)}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
