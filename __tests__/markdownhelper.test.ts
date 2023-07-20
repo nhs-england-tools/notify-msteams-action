4eef1d1cb04fa8b8c8169b2162b9e72e07313892
walteck <walteck@users.noreply.github.com>
2023-07-20 13:57:38 +0100

    resolving dependencies and prep for new release (#7)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    
    Uplifting out of date dependencies and uplifting to reference version
    0.0.2
    
    ## Context
    
    Resolving dependency issues and releasing new version.
    
    ## Type of changes
    
    - [ ] Refactoring (non-breaking change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [X] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [X] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [X] I have followed the code style of the project
    - [ ] I have added tests to cover my changes
    - [X] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [X] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

--- __tests__/markdownhelper.test.ts
+++ __tests__/markdownhelper.test.ts
@@ -2,32 +2,32 @@
 import {expect, test} from '@jest/globals'
 import {escapeMarkdown} from '../src/markdownhelper'
 test('test escaping markdown', async () => {
-    const testString = 'This is a test string'
-    const escapedString = escapeMarkdown(testString)
-    expect(escapedString).toBe(testString)
+  const testString = 'This is a test string'
+  const escapedString = escapeMarkdown(testString)
+  expect(escapedString).toBe(testString)
 })
 test('test escaping markdown with less than', async () => {
-    const testString = 'This is a test < string'
-    const escapedString = escapeMarkdown(testString)
-    expect(escapedString).toBe('This is a test &lt; string')
+  const testString = 'This is a test < string'
+  const escapedString = escapeMarkdown(testString)
+  expect(escapedString).toBe('This is a test &lt; string')
 })
 test('test escaping markdown with greater than', async () => {
-    const testString = 'This is a test > string'
-    const escapedString = escapeMarkdown(testString)
-    expect(escapedString).toBe('This is a test &gt; string')
+  const testString = 'This is a test > string'
+  const escapedString = escapeMarkdown(testString)
+  expect(escapedString).toBe('This is a test &gt; string')
 })
 test('test escaping markdown with ampersand', async () => {
-    const testString = 'This is a test & string'
-    const escapedString = escapeMarkdown(testString)
-    expect(escapedString).toBe('This is a test &amp; string')
+  const testString = 'This is a test & string'
+  const escapedString = escapeMarkdown(testString)
+  expect(escapedString).toBe('This is a test &amp; string')
 })
 test('test escaping markdown with single quote', async () => {
-    const testString = "This is a test ' string"
-    const escapedString = escapeMarkdown(testString)
-    expect(escapedString).toBe('This is a test &apos; string')
+  const testString = "This is a test ' string"
+  const escapedString = escapeMarkdown(testString)
+  expect(escapedString).toBe('This is a test &apos; string')
 })
 test('test escaping markdown with double quote', async () => {
-    const testString = 'This is a test " string'
-    const escapedString = escapeMarkdown(testString)
-    expect(escapedString).toBe('This is a test &quot; string')
+  const testString = 'This is a test " string'
+  const escapedString = escapeMarkdown(testString)
+  expect(escapedString).toBe('This is a test &quot; string')
 })
