f333df0b31cf6ea674a25a62d775fa49fee3bcf2
walteck <walteck@users.noreply.github.com>
2023-07-20 20:37:12 +0100

    converting to an adaptive card and adding link

--- src/messagecard.ts
+++ src/messagecard.ts
@@ -9,38 +9,110 @@ export function buildMessageCard(
   repoBranch: string,
   avatar_url: string,
   login: string,
-  author_url: string
+  author_url: string,
+  link: string
 ): string {
-  const card = {
-    '@type': 'MessageCard',
-    '@context': 'https://schema.org/extensions',
-    $schema: 'https://adaptivecards.io/schemas/adaptive-card.json',
-    version: '1.0',
-    summary: messageTitle,
-    themeColor: messageColour,
-    title: messageTitle,
-    sections: [
+  let actions: Record<string, string>[] = []
+  if (link !== '') {
+    actions = [
       {
-        activityTitle: `[${repoName}](${repoUrl})`,
-        activitySubtitle: `by [${login}](${author_url})`,
-        activityImage: avatar_url,
-        facts: [
-          {
-            name: 'Run Number',
-            value: runNumber
-          },
-          {
-            name: 'Run ID',
-            value: runId
-          },
-          {
-            name: 'Branch',
-            value: repoBranch
-          }
-        ],
-        text: messageBody
+        type: 'Action.OpenUrl',
+        title: 'View',
+        url: link,
+        role: 'button'
       }
     ]
   }
+
+  const title = {
+    type: 'TextBlock',
+    size: 'Large',
+    weight: 'Bolder',
+    text: messageTitle,
+    wrap: true
+  }
+
+  const subtitle = {
+    type: 'ColumnSet',
+    columns: [
+      {
+        type: 'Column',
+        width: 'auto',
+        items: [
+          {
+            type: 'Image',
+            url: avatar_url,
+            size: 'Small',
+            style: 'Person',
+            altText: login
+          }
+        ]
+      },
+      {
+        type: 'Column',
+        width: 'stretch',
+        items: [
+          {
+            type: 'TextBlock',
+            text: `[${repoName}](${repoUrl})`,
+            weight: 'Bolder',
+            size: 'Medium',
+            wrap: true
+          },
+          {
+            type: 'TextBlock',
+            text: `by [${login}](${author_url})`,
+            weight: 'Bolder',
+            size: 'Small',
+            wrap: true
+          }
+        ]
+      }
+    ]
+  }
+
+  const message = {
+    type: 'TextBlock',
+    text: messageBody,
+    wrap: true
+  }
+
+  const facts = {
+    type: 'FactSet',
+    facts: [
+      {
+        title: 'Run Number',
+        value: runNumber
+      },
+      {
+        title: 'Run ID',
+        value: runId
+      },
+      {
+        title: 'Branch',
+        value: repoBranch
+      }
+    ]
+  }
+
+  const adaptiveCard = {
+    type: 'AdaptiveCard',
+    $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+    version: '1.4',
+    body: [title, subtitle, message, facts],
+    actions
+  }
+
+  const card = {
+    type: 'message',
+    attachments: [
+      {
+        contentType: 'application/vnd.microsoft.card.adaptive',
+        contentUrl: null,
+        content: adaptiveCard
+      }
+    ]
+  }
+
   return JSON.stringify(card)
 }
