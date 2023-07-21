d4ecd25a8b88d8b9f346f3656a02bb1f4d7a01f2
walteck <walteck@users.noreply.github.com>
2023-07-21 15:13:08 +0100

    converting to an adaptive card and adding link (#9)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    
    Adding ability to display a link button through the notification in
    teams
    
    ## Context
    
    Enables users to optionally provide a link and to display this in the
    notification. Also switched to using AdaptiveCards for the incoming
    webhook format.
    
    ## Type of changes
    
    - [X] Refactoring (non-breaking change)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [X] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [X] I have followed the code style of the project
    - [X] I have added tests to cover my changes
    - [X] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [X] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -13,9 +13,10 @@ test('test building a message card', async () => {
   const repoName = 'XXXXXX'
   const repoUrl = 'XXXXXX'
   const repoBranch = 'XXXXXX'
-  const avatar_url = 'XXXXXX'
+  const avatar_url = 'https://avatars.githubusercontent.com/u/105098969'
   const login = 'XXXXXX'
   const author_url = 'XXXXXX'
+  const link = 'https://google.com'
   const message = buildMessageCard(
     messageTitle,
     messageBody,
@@ -27,8 +28,44 @@ test('test building a message card', async () => {
     repoBranch,
     avatar_url,
     login,
-    author_url
+    author_url,
+    link
   )
   const messageJson = JSON.parse(message)
-  expect(messageJson.summary).toBe(messageTitle)
+  expect(messageJson.type).toBe('message')
+  expect(messageJson.attachments[0].content.body[0].text).toBe(messageTitle)
+  expect(messageJson.attachments[0].content.actions[0].url).toBe(link)
+})
+
+test('test building a message card with no link', async () => {
+  const messageTitle = 'This is a title'
+  const messageBody = 'This is some text'
+  const messageColour = 'HEXCOL'
+  const runNumber = 'XXXXXX'
+  const runId = 'XXXXXX'
+  const repoName = 'XXXXXX'
+  const repoUrl = 'XXXXXX'
+  const repoBranch = 'XXXXXX'
+  const avatar_url = 'https://avatars.githubusercontent.com/u/105098969'
+  const login = 'XXXXXX'
+  const author_url = 'XXXXXX'
+  const link = ''
+  const message = buildMessageCard(
+    messageTitle,
+    messageBody,
+    messageColour,
+    runNumber,
+    runId,
+    repoName,
+    repoUrl,
+    repoBranch,
+    avatar_url,
+    login,
+    author_url,
+    link
+  )
+  const messageJson = JSON.parse(message)
+  expect(messageJson.type).toBe('message')
+  expect(messageJson.attachments[0].content.body[0].text).toBe(messageTitle)
+  expect(messageJson.attachments[0].content.actions).toStrictEqual([])
 })
