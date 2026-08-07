db8829c9a09d5a819bef0084b669a2968fb90403
susana-zhou1 <susana.zhou1@nhs.net>
2026-08-07 17:28:53 +0100

    ENg-1310 fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,26 +3,26 @@ import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default defineConfig([
-	{
-		// Note: there should be no other properties in this object
-		ignores: ["lib/**", "dist/**", "node_modules/**", "jest.config.js"],
-	},
-	{
-		files: ["src/**/*.ts"],
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				project: "./tsconfig.json",
-				sourceType: "module",
-				ecmaVersion: "latest",
-			},
-		},
-		plugins: {
-			"@typescript-eslint": tsPlugin,
-		},
-		rules: {
-			"no-unused-vars": "off",
-			"@typescript-eslint/no-unused-vars": "error",
-		},
-	},
+  {
+    // Note: there should be no other properties in this object
+    ignores: ["lib/**", "dist/**", "node_modules/**", "jest.config.js"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
 ]);
