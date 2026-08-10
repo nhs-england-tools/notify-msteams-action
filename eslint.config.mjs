a8f9284313c179fc6f94c7fea9d368df5f21c914
susana-zhou1 <susana.zhou1@nhs.net>
2026-08-10 11:19:21 +0100

    ENG-1310 add github plugin configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "eslint/config";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import githubPlugin from "eslint-plugin-github";
+import jestPlugin from "eslint-plugin-jest";
 
 export default defineConfig([
   {
     // Note: there should be no other properties in this object
     ignores: ["lib/**", "dist/**", "node_modules/**", "jest.config.js"],
+  },
+  {
+    ...githubPlugin.getFlatConfigs().recommended,
+    files: ["src/**/*.ts"],
   },
   {
     files: ["src/**/*.ts"],
@@ -16,13 +22,47 @@ export default defineConfig([
         sourceType: "module",
         ecmaVersion: "latest",
       },
+      globals: {
+        process: "readonly",
+      },
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
+      jest: jestPlugin,
     },
     rules: {
+      camelcase: "off",
+      "import/no-namespace": "off",
+      "i18n-text/no-en": "off",
+      "no-undef": "off",
       "no-unused-vars": "off",
+      "@typescript-eslint/array-type": "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/ban-ts-comment": "error",
+      "@typescript-eslint/consistent-type-assertions": "error",
+      "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
+      "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "no-public" }],
+      "@typescript-eslint/no-array-constructor": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-extraneous-class": "error",
+      "@typescript-eslint/no-for-in-array": "error",
+      "@typescript-eslint/no-inferrable-types": "error",
+      "@typescript-eslint/no-misused-new": "error",
+      "@typescript-eslint/no-namespace": "error",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-require-imports": "error",
+      "@typescript-eslint/no-unnecessary-qualifier": "error",
+      "@typescript-eslint/no-unnecessary-type-assertion": "error",
       "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-useless-constructor": "error",
+      "@typescript-eslint/prefer-for-of": "warn",
+      "@typescript-eslint/prefer-function-type": "warn",
+      "@typescript-eslint/prefer-includes": "error",
+      "@typescript-eslint/prefer-string-starts-ends-with": "error",
+      "@typescript-eslint/promise-function-async": "error",
+      "@typescript-eslint/require-array-sort-compare": "error",
+      "@typescript-eslint/restrict-plus-operands": "error",
+      "@typescript-eslint/unbound-method": "error",
     },
   },
 ]);
