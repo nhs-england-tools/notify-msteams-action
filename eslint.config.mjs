a2072b1dd85c4ec2066466fe3974a450db2ba892
susana-zhou1 <susana.zhou1@nhs.net>
2026-08-11 13:22:07 +0100

    ENG-1310 update packages and node (#261)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    
    <!-- Describe your changes in detail. -->
    Upgrade node to a supported version and also upgrade all the npm package
    dependencies to newer versions. which fixes existing vulnerabilities and
    keeps the code up to date
    
    ## Context
    
    <!-- Why is this change required? What problem does it solve? -->
    Node version deprecated and will soon not be supported on github actions
    anymore.
    
    ## Type of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply. -->
    
    - [x] Refactoring (non-breaking change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    - [ ] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [ ] I have followed the code style of the project
    - [ ] I have added tests to cover my changes
    - [ ] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [ ] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,48 @@
-{
-    "plugins": ["jest", "@typescript-eslint"],
-    "extends": ["plugin:github/recommended"],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-      "ecmaVersion": 9,
-      "sourceType": "module",
-      "project": "./tsconfig.json"
+import { defineConfig } from "eslint/config";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import githubPlugin from "eslint-plugin-github";
+import jestPlugin from "eslint-plugin-jest";
+
+export default defineConfig([
+  {
+    // Note: there should be no other properties in this object
+    ignores: ["lib/**", "dist/**", "node_modules/**", "jest.config.js"],
+  },
+  {
+    ...githubPlugin.getFlatConfigs().recommended,
+    files: ["src/**/*.ts"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+      globals: {
+        process: "readonly",
+      },
     },
-    "rules": {
-      "i18n-text/no-en": "off",
-      "eslint-comments/no-use": "off",
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      jest: jestPlugin,
+    },
+    rules: {
+      camelcase: "off",
       "import/no-namespace": "off",
+      "i18n-text/no-en": "off",
+      "no-undef": "off",
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/explicit-member-accessibility": ["error", {"accessibility": "no-public"}],
-      "@typescript-eslint/no-require-imports": "error",
       "@typescript-eslint/array-type": "error",
       "@typescript-eslint/await-thenable": "error",
       "@typescript-eslint/ban-ts-comment": "error",
-      "camelcase": "off",
       "@typescript-eslint/consistent-type-assertions": "error",
-      "@typescript-eslint/explicit-function-return-type": ["error", {"allowExpressions": true}],
-      "@typescript-eslint/func-call-spacing": ["error", "never"],
+      "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
+      "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "no-public" }],
       "@typescript-eslint/no-array-constructor": "error",
-      "@typescript-eslint/no-empty-interface": "error",
       "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/no-extraneous-class": "error",
       "@typescript-eslint/no-for-in-array": "error",
@@ -31,10 +50,11 @@
       "@typescript-eslint/no-misused-new": "error",
       "@typescript-eslint/no-namespace": "error",
       "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-require-imports": "error",
       "@typescript-eslint/no-unnecessary-qualifier": "error",
       "@typescript-eslint/no-unnecessary-type-assertion": "error",
+      "@typescript-eslint/no-unused-vars": "error",
       "@typescript-eslint/no-useless-constructor": "error",
-      "@typescript-eslint/no-var-requires": "error",
       "@typescript-eslint/prefer-for-of": "warn",
       "@typescript-eslint/prefer-function-type": "warn",
       "@typescript-eslint/prefer-includes": "error",
@@ -42,14 +62,7 @@
       "@typescript-eslint/promise-function-async": "error",
       "@typescript-eslint/require-array-sort-compare": "error",
       "@typescript-eslint/restrict-plus-operands": "error",
-      "semi": "off",
-      "@typescript-eslint/semi": ["error", "never"],
-      "@typescript-eslint/type-annotation-spacing": "error",
-      "@typescript-eslint/unbound-method": "error"
+      "@typescript-eslint/unbound-method": "error",
     },
-    "env": {
-      "node": true,
-      "es6": true,
-      "jest/globals": true
-    }
-  }
+  },
+]);
