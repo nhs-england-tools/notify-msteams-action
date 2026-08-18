b99675461e92f8ce015e5070c246b5e395d0ce7c
hirra-farooq <hirra.farooq1@nhs.net>
2026-08-18 09:54:55 +0100

    remove unused folders and examples

--- scripts/docker/docker.mk
+++ scripts/docker/docker.mk
@@ -40,44 +40,12 @@ docker-shellscript-lint: # Lint all Docker module shell scripts @Quality
 docker-test-suite-run: # Run Docker test suite @ExamplesAndTests
 	scripts/docker/tests/docker.test.sh
 
-docker-example-build: # Build Docker example @ExamplesAndTests
-	source scripts/docker/docker.lib.sh
-	cd scripts/docker/examples/python
-	DOCKER_IMAGE=repository-template/docker-example-python
-	DOCKER_TITLE="Repository Template Docker Python Example"
-	TOOL_VERSIONS="$(shell git rev-parse --show-toplevel)/scripts/docker/examples/python/.tool-versions.example"
-	docker-build
-
-docker-example-lint: # Lint Docker example @ExamplesAndTests
-	dockerfile=scripts/docker/examples/python/Dockerfile
-	file=$${dockerfile} scripts/docker/dockerfile-linter.sh
-
-docker-example-run: # Run Docker example @ExamplesAndTests
-	source scripts/docker/docker.lib.sh
-	cd scripts/docker/examples/python
-	DOCKER_IMAGE=repository-template/docker-example-python
-	args=" \
-		-it \
-		--publish 8000:8000 \
-	"
-	docker-run
-
-docker-example-clean: # Remove Docker example resources @ExamplesAndTests
-	source scripts/docker/docker.lib.sh
-	cd scripts/docker/examples/python
-	DOCKER_IMAGE=repository-template/docker-example-python
-	docker-clean
-
 # ==============================================================================
 
 ${VERBOSE}.SILENT: \
 	_docker \
 	clean \
 	docker-build \
-	docker-example-build \
-	docker-example-clean \
-	docker-example-lint \
-	docker-example-run \
 	docker-push \
 	docker-shellscript-lint \
 	docker-test-suite-run \
